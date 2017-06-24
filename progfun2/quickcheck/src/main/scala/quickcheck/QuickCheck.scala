ba636d73c3cf7dca24fb307f1820ac874bc627c6
pharron@gmail.com <pharron@gmail.com>
2017-06-24 15:22:43 +0100

    qc

--- progfun2/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ progfun2/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -11,7 +11,7 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   lazy val genHeap: Gen[H] = for {
     a <- arbitrary[Int]
-    h <- oneOf(value(empty), genHeap)
+    h <- oneOf(const(empty), genHeap)
   } yield insert(a, h)
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
@@ -26,14 +26,41 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(h) == a
   }
 
+  property("find min2") = forAll { (a: Int, b: Int) =>
+    val h1 = insert(a, empty)
+    val h2 = insert(b, h1)
+    findMin(h2) == scala.math.min(a, b)
+  }
 
-//  lazy val genMap: Gen[Map[Int,Int]] = oneOf(
-//    const(Map.empty[Int,Int]),
-//    for {
-//      k <- arbitrary[Int]
-//      v <- arbitrary[Int]
-//      m <- oneOf(const(Map.empty[Int,Int]), genMap)
-//    } yield m.updated(k, v)
-//  )
+  property("ins and delete") = forAll { a: Int =>
+    val h = insert(a, empty)
+    deleteMin(h) == empty
+  }
+
+  property("delete min") = forAll { (a: Int, b: Int, c: Int) =>
+    val h = insert(c, insert(b, insert(a, empty)))
+    findMin(deleteMin(deleteMin(h))) == scala.math.max(c, scala.math.max(a, b))
+  }
+
+  property("sorting a heap") = forAll { h: H =>
+    def isSorted(min: Int, h: H): Boolean =
+      if (isEmpty(h))
+        true
+      else if(min > findMin(h))
+        false
+      else
+        isSorted(findMin(h), deleteMin(h))
+
+    isSorted(findMin(h), deleteMin(h))
+  }
+
+  property("meld") = forAll { (h1: H, h2: H) =>
+    val m = meld(h1, h2)
+
+    val min = findMin(m)
+
+    min == findMin(h1) || min == findMin(h2)
+  }
+
 
 }
